676ff56211f124bf71ef781ed08aa3058f3e009a
John Hoang <67968051+jhoan2@users.noreply.github.com>
2021-06-23 12:06:25 -0700

    api requests will be authenticated if token there

--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import "./styles/index.css";
 import App from "./components/App";
 import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
-
+import { setContext } from "@apollo/client/link/context";
+import { AUTH_TOKEN } from "./constants";
 // 1
 import {
   ApolloProvider,
@@ -17,10 +18,18 @@ import {
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
-
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
 // 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
